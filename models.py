8e1e79860e0e51d89eaa9dcdaf7f1e5bd3ce84e6
18jayanth <jayanthtulugu@gmail.com>
2025-12-03 22:37:45 +0530

    bot is responding

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
-from openai import OpenAI
+# import requests
+
+# url = "https://raw.githubusercontent.com/groq/models/main/models.json"
+# models = requests.get(url).json()
+# print(models)
+from groq import Groq
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+load_dotenv()  
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+client = Groq(api_key=GROQ_API_KEY)
 
 models = client.models.list()
-
-for model in models.data:
-    print(model.id)
+for m in models.data:
+    print(m.id)
